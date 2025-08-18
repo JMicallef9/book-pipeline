72f5b860f405087ed95d6f0cb8e299bf9cf24bd6
Joseph Micallef <micallefje@gmail.com>
2025-08-18 11:30:47 +0100

    update data fetching functions

--- src/main.py
+++ src/main.py
@@ -9,4 +9,42 @@ def fetch_books_by_author(name):
     data = response.json()
     return data["docs"]
 
+def update_book_list(book_list):
+    book_details = []
+    for book in book_list:
+        book_dict = {}
+        book_dict["id"] = book["key"]
+        book_dict["title"] = book["title"]
+        book_dict["author_name"] = book["author_name"]
+        book_dict["first_publish_year"] = book["first_publish_year"]
+        book_dict["edition_count"] = book["edition_count"]
+        book_dict["language"] = book["language"]
+        book_details.append(book_dict)
+
+def fetch_book_details(book_key):
+    url = f"{URL}{book_key}.json"
+    response = requests.get(url)
+    response.raise_for_status()
+    data = response.json()
+    book_dict = {}
+    book_dict["subjects"] = data["subjects"]
+
+
+
+
 print(fetch_books_by_author("atwood"))
+
+
+# {
+# "id": "/works/OL82563W",
+# "title": "The Hobbit",
+# "author_name": ["J.R.R. Tolkien"],
+# "first_publish_year": 1937,
+# "edition_count": 300,
+# "subjects": ["Fantasy", "Adventure", "Hobbits"],
+# "language": ["eng"],
+# "publisher": ["George Allen & Unwin", "Houghton Mifflin"],
+# "isbn": ["9780007525492", "0261102214"]
+# }
+
+# /books/{edition_key}.json
