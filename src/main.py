e8fefb2e53bdd9a4de4ca0cb159c7e2bd1dc6f88
Joseph Micallef <micallefje@gmail.com>
2025-08-20 11:16:16 +0100

    update modular function

--- src/main.py
+++ src/main.py
@@ -15,24 +15,42 @@ def update_book_list(book_list):
         book_dict = {}
         book_dict["id"] = book["key"]
         book_dict["title"] = book["title"]
-        book_dict["author_name"] = book["author_name"]
-        book_dict["first_publish_year"] = book["first_publish_year"]
+        book_dict["author_name"] = book.get(["author_name"], [])
+        book_dict["first_publish_year"] = book.get(["first_publish_year"], [])
         book_dict["edition_count"] = book["edition_count"]
-        book_dict["language"] = book["language"]
+        book_dict["language"] = book.get(["language"], [])
         book_details.append(book_dict)
+    
+    return book_details
 
-def fetch_book_details(book_key):
+def fetch_book_subjects(book_key):
     url = f"{URL}{book_key}.json"
     response = requests.get(url)
     response.raise_for_status()
     data = response.json()
     book_dict = {}
-    book_dict["subjects"] = data["subjects"]
+    book_dict["subjects"] = data.get(["subjects"], [])
+    return book_dict
+
+def fetch_isbn_and_publisher_data(edition_key):
+    url = f"{URL}/books/{edition_key}.json"
+    response = requests.get(url)
+    response.raise_for_status()
+    data = response.json()
+    book_dict = {}
+    book_dict["publisher"] = data["publishers"]
+    book_dict["isbn"] = {
+        ["isbn_10"]: data.get(["isbn_10"], []),
+        ["isbn_13"]: data.get(["isbn_13"], [])
+    }
+    return book_dict
 
 
+def get_cover_edition_key(book):
+    return book.get("cover_edition_key") or (book.get("edition_key", [None])[0])
 
 
-print(fetch_books_by_author("atwood"))
+fetch_books_by_author("atwood")
 
 
 # {
@@ -46,5 +64,3 @@ print(fetch_books_by_author("atwood"))
 # "publisher": ["George Allen & Unwin", "Houghton Mifflin"],
 # "isbn": ["9780007525492", "0261102214"]
 # }
-
-# /books/{edition_key}.json
